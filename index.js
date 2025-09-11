dbe300d2ce988b2aa81a14804734f08ff0f198bb
User <User@email.com>
2025-09-11 18:59:57 -0500

    modificacion

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const http = require('http');
 
 const server = http.createServer(function(req, res){
-    res.writeHead(200, {'content-type'; 'text/plain'});
+    res.writeHead(200, {'content-type': 'text/plain'});
     res.end('Hola mundo');
 
 });
