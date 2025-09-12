19ff490d3ae068ee42b61eab47329896417d592e
User <User@email.com>
2025-09-11 19:36:41 -0500

    modificacion que añade muchas cosas

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
-'use strict';
-const http = require('http');
-
-const server = http.createServer(function(req, res){
-    res.writeHead(200, {'content-type': 'text/plain'});
-    res.end('Hola mundo');
-
-});
-server.listen(5000);
+//'use strict';
+//const http = require('http');
+//
+//const server = http.createServer(function(req, res){
+//    res.writeHead(200, {'content-type': 'text/plain'});
+//    res.end('Hola mundo');
+//
+//});
+//server.listen(5000);
 
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3000                 //el puerto que se va a poner en el navegador
 
-app.get ('/', (req, res) =>{
-    res.send('Hola')
+app.get ('/', (req, res) =>{         
+    res.send('Hola')              //rutas como tal
 })
 
 app.listen(port, () =>{
-    console.log('la app se está ejecutando')
+    console.log('la app se está ejecutando')    //mensaje que muestra cuando se ejecuta
 })
+
+
